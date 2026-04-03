caf8aede4f2a3377aef62ca9efbdc5e0c2c4f098
Anirban <asanirban298@gmail.com>
2026-04-03 12:51:30 +0530

    CICD commit 3

--- src/test/java/anirbansarkar/tests/SubmitOrderTest.java
+++ src/test/java/anirbansarkar/tests/SubmitOrderTest.java
@@ -16,7 +16,7 @@ import anirbansarkar.pageObjects.ProductCatalogue;
 
 public class SubmitOrderTest extends BaseTest {
 
-//	Change to trigger 1st CICD event
+//	Change to trigger 2nd CICD event
 //	String email = "asanirban298@gmail.com";
 //	String pwd = "sA2!091959";
 //	String order = "ZARA COAT 3";
